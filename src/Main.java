bab73f5c62b624018944f77556b9b359a6f05b8f
gKseni <gusevako@gmail.com>
2017-06-22 21:54:58 +0400

    add regex checker, check of files format, change split symbol

--- src/Main.java
+++ src/Main.java
@@ -9,25 +9,30 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class Main {
-
     public static void main(String[] args) {
         final String patternsFileName = "patterns.txt";
+        final String regexsFileName = "regexes.txt";
         Map<String, String> patterns = readPatterns(patternsFileName);
+        Map<String, String> regexes = readPatterns(regexsFileName);
 
         final File folder = new File("main");
-        analyzeFilesFromFolder(folder, patterns);
+        analyzeFilesFromFolder(folder, patterns, regexes);
 
     }
 
-    public static void analyzeFilesFromFolder(File folder, Map<String, String> patterns) {
+    public static void analyzeFilesFromFolder(File folder, Map<String, String> patterns, Map<String, String> regexes) {
         File[] folderEntries = folder.listFiles();
-        for (File entry : folderEntries) { //if folder - next step
+        for (File entry : folderEntries) {
             if (entry.isDirectory()) {
-                analyzeFilesFromFolder(entry, patterns);
+                analyzeFilesFromFolder(entry, patterns, regexes);
                 continue;
             }
-            // if file
-            findPatternsInFile(entry.getAbsolutePath(), patterns);
+
+            String filePath = entry.getAbsolutePath();
+            if(filePath.contains(".java") || filePath.contains(".xml") ||
+                    filePath.contains(".class") || filePath.contains(".js")){
+                findPatternsAndRegexesInFile(filePath, patterns, regexes);
+            }
         }
     }
 
@@ -44,12 +49,9 @@ public class Main {
             String line;
 
             while ((line = br.readLine()) != null) {
-                String textToFind = line.substring(0, line.indexOf(':'));
-                String message = line.substring(line.indexOf(':') + 1);
+                String textToFind = line.substring(0, line.indexOf('~'));
+                String message = line.substring(line.indexOf('~') + 1);
                 patterns.put(textToFind, message);
-//                System.out.println("Text to find: " + textToFind);
-//                System.out.println("Message: " + message);
-//                System.out.println();
             }
 
         } catch (IOException e) {
@@ -59,7 +61,7 @@ public class Main {
         return patterns;
     }
 
-    public static void findPatternsInFile(String fileName, Map<String, String> patterns) {
+    public static void findPatternsAndRegexesInFile(String fileName, Map<String, String> patterns,  Map<String, String> regexes) {
         BufferedReader br = null;
         FileReader fr = null;
 
@@ -76,14 +78,25 @@ public class Main {
             Integer cnt = 1;
             while ((line = br.readLine()) != null) {
                 for (String pattern : patterns.keySet()) {
-
                     Pattern p = Pattern.compile(pattern);
-                    Matcher m = p.matcher(line);
+                    Matcher mp = p.matcher(line);
 
-                    if(m.find()) {
+                    if(mp.find()) {
                         System.out.println("Find in string #" + cnt + ":");
                         System.out.println(pattern);
                         System.out.println(patterns.get(pattern));
+                        System.out.println();
+                    }
+                }
+
+                for (String regex : regexes.keySet()) {
+                    Pattern r = Pattern.compile(regex);
+                    Matcher mr = r.matcher(line);
+
+                    if(mr.find()) {
+                        System.out.println("Find in string #" + cnt + ":");
+                        System.out.println(mr.group());
+                        System.out.println(regexes.get(regex));
                         System.out.println();
                     }
 
