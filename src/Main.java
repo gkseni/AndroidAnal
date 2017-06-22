96ece8c8c12ae6373aac53531af6d7802d7142fc
gKseni <gusevako@gmail.com>
2017-06-22 16:48:48 +0400

    Add 1)parse pattern file 2)make pattern map 3)find pattern in file 4)open files in folder

--- src/Main.java
+++ src/Main.java
@@ -1,30 +1,92 @@
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class Main {
 
     public static void main(String[] args) {
-       String s = "<uses-permission android:name=\"android.permission.READ_CONTACTS\"/>\n" +
-               "<uses-permission android:name=\"android.permission.WRITE_CONTACTS\"/>\n" +
-               "<uses-permission android:name=\"android.permission.RECEIVE_BOOT_COMPLETED\"/>\n" +
-               "...\n" +
-               "<receiver android:name=\"android.trojan.ExterminateModule\">\n" +
-               "    <intent-filter>\n" +
-               "        <action android:name=\"android.trojan.action.EXTERMINATE\"/>\n" +
-               "    </intent-filter>\n" +
-               "</receiver>\n" +
-               "<receiver android:name=\"android.trojan.VirusController\">\n" +
-               "    <intent-filter>\n" +
-               "        <action android:name=\"android.intent.action.BOOTCOMPLETED\"/>\n" +
-               "    </intent-filter>\n" +
-               "</receiver>";
 
-        Pattern pattern = Pattern.compile("android\\.permission\\.[A-Z,_]+");
-        Matcher matcher = pattern.matcher(s);
+        final String patternsFileName = "patterns.txt";
+        Map<String, String> patterns = readPatterns(patternsFileName);
 
-        while (matcher.find()) {
-            System.out.println(matcher.group());
+        final File folder = new File("testFolder");
+        File[] files = folder.listFiles();
+
+        for (File file : files) {
+            findPatternsInFile(file.getAbsolutePath(), patterns);
         }
 
+    }
+
+    public static Map<String, String> readPatterns(String fileName) {
+        BufferedReader br = null;
+        FileReader fr = null;
+
+        Map<String, String> patterns = new HashMap<>();
+
+        try {
+
+            fr = new FileReader(fileName);
+            br = new BufferedReader(fr);
+
+            String line;
+
+            while ((line = br.readLine()) != null) {
+                String textToFind = line.substring(0, line.indexOf(':'));
+                String message = line.substring(line.indexOf(':') + 1);
+                patterns.put(textToFind, message);
+//                System.out.println("Text to find: " + textToFind);
+//                System.out.println("Message: " + message);
+//                System.out.println();
+            }
+
+        } catch (IOException e) {
+            System.out.println("Something went wrong");
+        }
+
+        return patterns;
+    }
+
+    public static void findPatternsInFile(String fileName, Map<String, String> patterns) {
+        BufferedReader br = null;
+        FileReader fr = null;
+
+        try {
+
+            fr = new FileReader(fileName);
+            br = new BufferedReader(fr);
+
+            String line;
+
+            System.out.println("Searching in file '" + fileName + "'" );
+            System.out.println();
+
+            Integer cnt = 1;
+            while ((line = br.readLine()) != null) {
+                for (String pattern : patterns.keySet()) {
+
+                    Pattern p = Pattern.compile(pattern);
+                    Matcher m = p.matcher(line);
+
+                    if(m.find()) {
+                        System.out.println("Find in string #" + cnt + ":");
+                        System.out.println(pattern);
+                        System.out.println(patterns.get(pattern));
+                        System.out.println();
+                    }
+
+                }
+                cnt++;
+            }
+
+        } catch (IOException e) {
+            System.out.println("Something went wrong");
+        }
     }
 }
